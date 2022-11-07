aad6afb01e4decc70dca8dae36522e27e8b3bb83
Aleksey <aleksey18rus@inbox.ru>
2022-11-07 15:39:42 +0300

    flake8 correction

--- test/mysqllib.py
+++ test/mysqllib.py
@@ -1,11 +1,15 @@
-import domain
 import sqlalchemy as db
 from secrets import token_hex
 from sqlalchemy.orm import Session
 
+import domain
+
+
 class mysqllib:
     def __init__(self, host, port, user, password, database):
-        self.engine = db.create_engine("mysql+pymysql://{}:{}@{}:{}/{}".format(user, password, host, port,database))
+        self.engine = db.create_engine("mysql+pymysql://{}:{}@{}:{}/{}"
+                                       .format(user, password,
+                                               host, port, database))
         self.meta = db.MetaData()
         print("Connected to mysql")
 
@@ -16,7 +20,8 @@ class mysqllib:
             session.add(request)
             session.commit()
             return request
-        except:
+        except Exception as e:
+            print(e)
             session.rollback()
         finally:
             session.close()
@@ -26,60 +31,66 @@ class mysqllib:
         token = token_hex(50)
         session = Session(self.engine)
         try:
-            session.add(domain.User(first_token = token))
+            session.add(domain.User(first_token=token))
             session.commit()
             return token
-        except:
+        except Exception as e:
+            print(e)
             session.rollback()
         finally:
             session.close()
 
-    """Check if id of users tg exists in database if exists: return user else: return False"""
+    """Check if id of user telegram exists in database"""
     def userAuthorization(self, tg_id):
         session = Session(self.engine)
         try:
-            user = session.query(domain.User).filter(domain.User.tg_id == tg_id).first()
+            user = session.query(domain.User)\
+                .filter(domain.User.tg_id == tg_id).first()
             if not user:
                 return False
             return user
-        except:
-            print("exception in userAuthorization")
+        except Exception as e:
+            print(e)
         finally:
             session.close()
 
-    """Check if token exists in database and do not have connection with any account create new user and return him.
-    otherwise return false"""
+    """Try to find token in database and create a new user if successes"""
     def tokenCheck(self, user: domain.User):
         session = Session(self.engine)
         try:
-            userFromDb = session.query(domain.User).filter(domain.User.first_token == user.first_token).first()
+            tokenFromDb: domain.User = session.query(domain.User)\
+                .filter(domain.User.first_token == user.first_token).first()
 
-            if not userFromDb:
+            if not tokenFromDb:
                 return False
-            if userFromDb.tg_id != 0:
-                userFromDb.token_requests_count = userFromDb.token_requests_count + 1
+            if tokenFromDb.tg_id != 0:
+                tokenFromDb.token_requests_count += 1
                 session.commit()
                 return False
-            userFromDb.create_dt = user.create_dt
-            userFromDb.tg_id = user.tg_id
-            userFromDb.username = user.username
-            userFromDb.descrtext = user.descrtext
-            userFromDb.token_requests_count = userFromDb.token_requests_count
+            tokenFromDb.create_dt = user.create_dt
+            tokenFromDb.tg_id = user.tg_id
+            tokenFromDb.username = user.username
+            tokenFromDb.descrtext = user.descrtext
+            tokenFromDb.token_requests_count = tokenFromDb.token_requests_count
             session.commit()
-            return userFromDb
-        except:
+            user.id = tokenFromDb.id
+            user.token_requests_count = tokenFromDb.token_requests_count
+            return user
+        except Exception as e:
+            print(e)
             session.rollback()
         finally:
             session.close()
-            
-    """add notify to database"""
-    def notifyAdd(self, notify:domain.Notify):
+
+    """Add notify to database"""
+    def notifyAdd(self, notify: domain.Notify):
         session = Session(self.engine)
         try:
             session.add(notify)
             session.commit()
             return notify
-        except:
+        except Exception as e:
+            print(e)
             session.rollback()
         finally:
             session.close()
@@ -87,14 +98,16 @@ class mysqllib:
     def notifyGetAll(self):
         session = Session(self.engine)
         try:
-            notifications = session.query(domain.Notify).filter(domain.Notify.sended == 0).all()
+            notifications = session.query(domain.Notify)\
+                .filter(domain.Notify.sended == 0).all()
             return notifications
-        except:
+        except Exception as e:
+            print(e)
             session.rollback()
         finally:
             session.close()
-    
-    """Change notify.sended to 1"""
+
+    """Change notify status to sent"""
     def notifySended(self, arr):
         session = Session(self.engine)
         try:
@@ -102,34 +115,35 @@ class mysqllib:
                 notifications = session.query(domain.Notify).get(n)
                 notifications.sended = 1
                 session.commit()
-        except:
+        except Exception as e:
+            print(e)
             session.rollback()
         finally:
             session.close()
 
     def createTableUsers(self):
         query = "CREATE TABLE `users` (id int AUTO_INCREMENT NOT NULL,"\
-        "create_dt TIMESTAMP NOT NULL DEFAULT NOW(),"\
-        "tg_id INT NOT NULL DEFAULT 0,"\
-        "username TEXT,"\
-        "descrtext TEXT,"\
-        "first_token TEXT,"\
-        "token_requests_count INT NOT NULL DEFAULT 0,"\
-        "PRIMARY KEY(id))"
+                "create_dt TIMESTAMP NOT NULL DEFAULT NOW(),"\
+                "tg_id INT NOT NULL DEFAULT 0,"\
+                "username TEXT,"\
+                "descrtext TEXT,"\
+                "first_token TEXT,"\
+                "token_requests_count INT NOT NULL DEFAULT 0,"\
+                "PRIMARY KEY(id))"
         self.myCon.execute(query)
 
     def createTableRequests(self):
         query = "CREATE TABLE `requests` (id int AUTO_INCREMENT NOT NULL,"\
-        "tg_id INT NOT NULL DEFAULT 0,"\
-        "ts TIMESTAMP NOT NULL DEFAULT NOW(),"\
-        "request_text TEXT,"\
-        "PRIMARY KEY(id))"
+                "tg_id INT NOT NULL DEFAULT 0,"\
+                "ts TIMESTAMP NOT NULL DEFAULT NOW(),"\
+                "request_text TEXT,"\
+                "PRIMARY KEY(id))"
         self.myCon.execute(query)
 
     def createTableNotify(self):
         query = "CREATE TABLE `notify` (id int AUTO_INCREMENT NOT NULL,"\
-        "tg_id INT NOT NULL DEFAULT 0,"\
-        "notify_text TEXT,"\
-        "sended SMALLINT NOT NULL DEFAULT 0,"\
-        "PRIMARY KEY(id))"
+                "tg_id INT NOT NULL DEFAULT 0,"\
+                "notify_text TEXT,"\
+                "sended SMALLINT NOT NULL DEFAULT 0,"\
+                "PRIMARY KEY(id))"
         self.myCon.execute(query)
--- test/test.py
+++ test/test.py
@@ -1,86 +1,83 @@
-from ctypes.wintypes import INT
-from datetime import datetime
-import mysqllib
 import telebot
 import yaml
-import domain
-#
 import threading
+
+from datetime import datetime
+
+import mysqllib
+import domain
+
 
 try:
     with open("Consts.yaml", "r") as yam:
         consts = yaml.safe_load(yam)
-except Error (e):
-    print("Can't read config")
-    print(e)
+except Exception as e:
+    print("Config 'Consts.yaml' do no read", e)
 
-try:
-    myBase = mysqllib.mysqllib(consts['host'], consts['port'], consts['user'], consts['password'], consts['database'])
-except Error (e):
-    print("Base crashed")
-    print(e)
+myBase = mysqllib.mysqllib(consts['host'], consts['port'],
+                           consts['user'], consts['password'],
+                           consts['database'])
 
 bot = telebot.TeleBot(consts['token'])
 
-#############################################################
-#myBase.addToken()
-#myBase.createTableUsers()
-#myBase.createTableRequests()
-#myBase.createTableNotify()
-#myBase.notifyAdd(domain.Notify(notify_text = "test2"))
-#print (myBase.addToken())
-#myBase.userAddTokenRequest(domain.User(id = 2, create_dt = '2022-11-02 12:33:00', tg_id = 1855954960, username = 'AlexEltc', descrtext = "test", token_requests_count = 0))
-#print(myBase.userAuthorization(1855954960).id)
-#############################################################
 
 @bot.message_handler(content_types=['text'])
 def Request(message):
-    formatted_date = datetime.utcfromtimestamp(int(message.date)).strftime('%Y-%m-%d %H:%M:%S')
-    print("Message Received: UserId: {}, Date: {}, MessageText: {}.".format(message.chat.id, formatted_date, message.text))
+    formatted_date = datetime.utcfromtimestamp(int(message.date))\
+        .strftime('%Y-%m-%d %H:%M:%S')
+    print("Message Received: UserId: {}, Date: {}, MessageText: {}."
+          .format(message.chat.id, formatted_date, message.text))
 
-    try:    
-        authorizedUser = myBase.userAuthorization(message.chat.id)
-        if authorizedUser == False:
-            newUser = myBase.tokenCheck(
-                domain.User(create_dt = formatted_date, 
-                            tg_id = message.chat.id, 
-                            username = message.from_user.username, 
-                            descrtext = "", first_token = message.text,)
-                )
-            if newUser == False:
-                return
-        elif authorizedUser.token_requests_count >= consts['max_token_request']:
+    authorizedUser = myBase.userAuthorization(message.chat.id)
+    if not authorizedUser:
+        newUser = myBase.tokenCheck(domain.User(
+            create_dt=formatted_date,
+            tg_id=message.chat.id,
+            username=message.from_user.username,
+            descrtext="",
+            first_token=message.text,))
+        if not newUser:
             return
-    except Error(e):
-        print("Authorization authentication error")
-        print(e)
+        authorizedUser = newUser
+    elif authorizedUser.token_requests_count >= consts['max_token_request']:
+        return
 
-    myBase.insertRequest(domain.Request(tg_id = message.chat.id,ts = formatted_date, request_text = message.text))
+    myBase.insertRequest(domain.Request(tg_id=message.chat.id,
+                                        ts=formatted_date,
+                                        request_text=message.text))
 
-    bot.send_message(message.chat.id, message.text) 
+    bot.send_message(message.chat.id, message.text)
+
 
 class Notifyer(object):
-    loop = consts['notifyerLoopTime']                                                   
+    _timeBetweenNotify = consts['timeBetweenNotify']
+
     def changeTime(self, time):
-        self.loop = time
-     
+        self._timeBetweenNotify = time
+
     def startNotifyLoop(self):
-        threading.Timer(self.loop, self.startNotifyLoop).start()
+        threading.Timer(self._timeBetweenNotify, self.startNotifyLoop).start()
         notifications = myBase.notifyGetAll()
+        if not notifications:
+            return
+
         sended = []
         for n in notifications:
             try:
                 bot.send_message(n.tg_id, n.notify_text)
                 sended.append(n.id)
-            except:
+            except Exception as e:
                 print("not sent: {}".format(n.id))
+                print(e)
         print("Notifications sent")
         myBase.notifySended(sended)
+
 
 notifyer = Notifyer()
 notifyer.startNotifyLoop()
 
-bot.polling(none_stop=True, timeout=123)
-
-
-
+while True:
+    try:
+        bot.polling(none_stop=True, timeout=10)
+    except Exception as e:
+        print(e)
