2924de1b0417166bce99e64c66ac5b06bd76d15a
Aleksey <aleksey18rus@inbox.ru>
2022-11-10 13:24:54 +0300

    Requests and Notify add

--- taskmanager/telegramBot/admin.py
+++ taskmanager/telegramBot/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 
 from .models import Users
+from .models import Requests
+from .models import Notify
 
 
 @admin.register(Users)
 class UsersAdmin(admin.ModelAdmin):
-    list_display = ('id', 'tg_id', 'username')
+    list_display = ('id', 'tg_id', 'username',
+                    'descrtext', 'first_token', 'token_requests_count')
+
+
+@admin.register(Requests)
+class RequestsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tg_id', 'ts', 'request_text')
+
+
+@admin.register(Notify)
+class NotifyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tg_id', 'notify_text', 'sended')
